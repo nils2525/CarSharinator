5ee0e133447e74da64d293dc855c81d87d9f9bd5
Narandum <blabla@blabla.bla>
2019-11-22 10:45:04 +0100

    Projekt erstellt in Netbeans

---
+++ src/main/java/com/carsharinator/app/CarSharinatorApplication.java
@@ -0,0 +1,13 @@
+package com.carsharinator.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSharinatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarSharinatorApplication.class, args);
+	}
+
+}
