63092de51b2d24ad004edd571d16168d63cf1e59
Kay Joe Kahmann <kay.kahmann@gmail.com>
2019-11-22 11:10:30 +0100

    fixed package name in User.java

--- src/main/java/com/carsharinator/app/models/users/User.java
+++ src/main/java/com/carsharinator/app/models/users/User.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.carsharinator.app.models;
+package com.carsharinator.app.models.users;
 
 import java.util.Date;
 
